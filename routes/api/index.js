f6a818fdb18d0d662b1faeed19e71dadcabc5bfd
SMarquis85 <sophienathalie_85@outlook.com>
2023-08-11 20:19:31 -0400

    updated routes folder, created index.js file

---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const movieRoutes = require('./movie-routes'); // Update to use movie-routes
+const actorRoutes = require('./actor-routes'); // Add actor-routes
+const characterRoutes = require('./character-routes'); // Add character-routes
+const categoryRoutes = require('./category-routes');
+
+// All of these routes are prefixed with '/api'
+router.use('/movies', movieRoutes); // Update to use movieRoutes
+router.use('/actors', actorRoutes); // Add actorRoutes
+router.use('/characters', characterRoutes); // Add characterRoutes
+router.use('/categories', categoryRoutes);
+
+module.exports = router;
