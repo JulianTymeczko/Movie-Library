333b11147ff01139c7f980801467e5f084ba14ad
SMarquis85 <sophienathalie_85@outlook.com>
2023-08-11 21:10:08 -0400

    updated character-seeds.js file

---
+++ seeds/character-seeds.js
@@ -0,0 +1,23 @@
+const { Character } = require('../models'); // Make sure to use the correct model name
+
+const characterData = [
+  {
+    character_name: 'Andy Dufresne', // Character in Shawshank Redemption
+  },
+  {
+    character_name: 'Jack Torrance', // Character in The Shining
+  },
+  {
+    character_name: 'Carrie White', // Character in Carrie
+  },
+  {
+    character_name: 'Pennywise', // Character in It
+  },
+  {
+    character_name: 'Gordie Lachance', // Character in Stand by Me
+  },
+];
+
+const seedCharacters = () => Character.bulkCreate(characterData);
+
+module.exports = seedCharacters;
