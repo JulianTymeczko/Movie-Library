afdc03f425da3f14bf1087cb8c46d7e60ff991d5
SMarquis85 <sophienathalie_85@outlook.com>
2023-08-28 22:26:02 -0400

    updated home-routes.js file

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -20,8 +20,11 @@ const data = [{
 });
 */
 
-router.get('/', (req, res) => {
-  res.render('index');
+router.get('/', async (req, res) => {
+  // TODO: double check if this is correct.
+  const result = await Movie.findAll();
+  const movies = result.map(x => x.get({plain: true}));
+  res.render('index', { movies });
 })
 
 router.get('/signup', async (req, res) => {
@@ -29,10 +32,35 @@ router.get('/signup', async (req, res) => {
   res.render('signup');
 });
 
-router.get('/login', async (req, res) => {
- 
-  res.render('Login');
+router.post('/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!dbUserData) {
+      return res.status(400).json({ message: 'Incorrect email or password.' });
+    }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      return res.status(400).json({ message: 'Incorrect email or password.' });
+    }
+
+    req.session.loggedIn = true; // Set the loggedIn property in the session
+    // You can set more session properties if needed
+
+    return res.status(200).json({ message: 'Login successful.' });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'An error occurred.' });
+  }
 });
+
+module.exports = router;
 
 router.get('/contact', async (req, res) => {
  
