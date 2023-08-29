218de475179cfea3f1e508148d28a0d92aace4ed
Julian Houle-T <jhouletymeczko@gmail.com>
2023-08-29 00:29:38 -0400

    fixed issue of images not loading

--- models/index.js
+++ models/index.js
@@ -1,36 +1,36 @@
 // Import models
 const Movie = require('./Movie');
-const Actor = require('./Actor');
-const Character = require('./Character');
-const Category = require('./Category'); // Add this line to import the Category model
+// const Actor = require('./Actor');
+// const Character = require('./Character');
+// const Category = require('./Category'); // Add this line to import the Category model
 
 // Movies belongToMany Actors (through Character)
-Movie.belongsToMany(Actor, {
-  through: Character,
-  foreignKey: 'movie_id'
-});
+// Movie.belongsToMany(Actor, {
+//   through: Character,
+//   foreignKey: 'movie_id'
+// });
 
-// Actors belongToMany Movies (through Character)
-Actor.belongsToMany(Movie, {
-  through: Character,
-  foreignKey: 'actor_id'
-});
+// // Actors belongToMany Movies (through Character)
+// Actor.belongsToMany(Movie, {
+//   through: Character,
+//   foreignKey: 'actor_id'
+// });
 
-// Movies belong to Categories
-Movie.belongsTo(Category, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
-});
+// // Movies belong to Categories
+// Movie.belongsTo(Category, {
+//   foreignKey: 'category_id',
+//   onDelete: 'CASCADE',
+// });
 
-// Categories have many Movies
-Category.hasMany(Movie, {
-  foreignKey: 'category_id',
-  // onDelete: 'CASCADE',
-});
+// // Categories have many Movies
+// Category.hasMany(Movie, {
+//   foreignKey: 'category_id',
+//   // onDelete: 'CASCADE',
+// });
 
 module.exports = {
   Movie,
-  Actor,
-  Character,
-  Category // Add Category to the exported object
+  // Actor,
+  // Character,
+  // Category // Add Category to the exported object
 }; 
