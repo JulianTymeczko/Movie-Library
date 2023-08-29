650058b4f724b7dc16862944ed9b67a1d8da2937
SMarquis85 <sophienathalie_85@outlook.com>
2023-08-28 22:31:29 -0400

    updated home-routes.js file

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -1,37 +1,57 @@
 const { Movie } = require('../models');
 
 const router = require('express').Router();
+/*
+router.get('/', async (req, res) => {
+
+// const movies = await Movie.findAll();
+
+// const data = movies.map(movie => movie.get({plain: true}))
+const data = [{
+    title: "IT",
+    image: "/images/carrie.jpg"
+  },
+  {
+    title: "Carrie",
+    image: "/images/carrie.jpg"
+  }]
+  res.render('index', {data});
+   
+});
+*/
 
 router.get('/', async (req, res) => {
+  // TODO: double check if this is correct.
   const result = await Movie.findAll();
-  const movies = result.map(x => x.get({ plain: true }));
-  res.render('index', { movies });
-});
+  const movies = result.map(x => x.get({plaine: true}));
+  res.render('index', {movies });
+})
 
 router.get('/signup', async (req, res) => {
+ 
   res.render('signup');
 });
 
-router.post('/login', async (req, res) => {
-  try {
-    // ... your login code ...
-    return res.status(200).json({ message: 'Login successful.' });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ message: 'An error occurred.' });
-  }
+router.get('/login', async (req, res) => {
+ 
+  res.render('Login');
 });
 
 router.get('/contact', async (req, res) => {
+ 
   res.render('contact');
 });
-
 router.get('/aboutus', async (req, res) => {
+ 
   res.render('aboutus');
 });
 
 router.get('/movies', (req, res) => {
   res.render('landing');
-});
+})
+
+
+
 
 module.exports = router;
+
